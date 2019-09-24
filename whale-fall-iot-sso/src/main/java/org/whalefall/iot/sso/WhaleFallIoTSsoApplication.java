d7fc40211b7a8a016e54672b49379049db9f8636
YoXung <89034550@qq.com>
2019-09-25 07:06:35 +0800

    管理端单点登陆

---
+++ whale-fall-iot-sso/src/main/java/org/whalefall/iot/sso/WhaleFallIoTSsoApplication.java
@@ -0,0 +1,13 @@
+package org.whalefall.iot.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleFallIoTSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhaleFallIoTSsoApplication.class, args);
+    }
+
+}
