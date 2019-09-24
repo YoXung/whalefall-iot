c7e21c72d57fba9a2d3c1cacbe02624e7b3b37fc
YoXung <89034550@qq.com>
2019-09-25 00:36:18 +0800

    新增应用中心平台服务

---
+++ whale-fall-iot-acp/src/main/java/org/whalefall/io/acp/WhaleFallIoTAcpApplication.java
@@ -0,0 +1,13 @@
+package org.whalefall.io.acp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleFallIoTAcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhaleFallIoTAcpApplication.class, args);
+    }
+
+}
