70c7234bdc17d1c1530e3f1ac7d147223adde3c4
YaoXiang <89034550@qq.com>
2019-10-11 11:04:46 +0800

    新增cig鉴权模块

---
+++ whale-fall-iot-cig/src/main/java/org/whalefall/iot/cig/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.whalefall.iot.cig;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WhaleFallIotCigApplication.class);
+    }
+
+}
---
+++ whale-fall-iot-cig/src/main/java/org/whalefall/iot/cig/WhaleFallIotCigApplication.java
@@ -0,0 +1,13 @@
+package org.whalefall.iot.cig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleFallIotCigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhaleFallIotCigApplication.class, args);
+    }
+
+}
