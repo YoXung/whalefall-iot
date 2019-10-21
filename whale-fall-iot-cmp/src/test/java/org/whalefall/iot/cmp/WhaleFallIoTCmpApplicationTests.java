d14f923b1b8a9453825901f8cb91881e96494f0b
YaoXiang <89034550@qq.com>
2019-10-21 11:08:14 +0800

    升级spring boot测试类

--- whale-fall-iot-cmp/src/test/java/org/whalefall/iot/cmp/WhaleFallIoTCmpApplicationTests.java
+++ whale-fall-iot-cmp/src/test/java/org/whalefall/iot/cmp/WhaleFallIoTCmpApplicationTests.java
@@ -1,16 +1,13 @@
 package org.whalefall.iot.cmp;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class WhaleFallIoTCmpApplicationTests {
+class WhaleFallIoTCmpApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
