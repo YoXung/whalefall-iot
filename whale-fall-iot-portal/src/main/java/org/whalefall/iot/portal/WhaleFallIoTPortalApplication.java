1e3497faae2f1609ce7086c01f94e5f8231c36d9
YaoXiang <89034550@qq.com>
2020-01-14 16:44:48 +0800

    1、升级spring cloud版本 2、新增portal前端聚合服务

---
+++ whale-fall-iot-portal/src/main/java/org/whalefall/iot/portal/WhaleFallIoTPortalApplication.java
@@ -0,0 +1,13 @@
+package org.whalefall.iot.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleFallIoTPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhaleFallIoTPortalApplication.class, args);
+    }
+
+}
---
+++ whale-fall-iot-portal/src/test/java/org/whalefall/iot/portal/WhaleFallIoTPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.whalefall.iot.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhaleFallIoTPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
