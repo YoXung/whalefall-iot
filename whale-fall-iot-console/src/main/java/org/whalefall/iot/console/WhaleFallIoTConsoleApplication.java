8937d9894781d5cf6b2d6a4127d2532e0f37d2e5
YoXung <89034550@qq.com>
2020-02-10 19:13:33 +0800

    新增console

---
+++ whale-fall-iot-console/src/main/java/org/whalefall/iot/console/WhaleFallIoTConsoleApplication.java
@@ -0,0 +1,13 @@
+package org.whalefall.iot.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleFallIoTConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhaleFallIoTConsoleApplication.class, args);
+    }
+
+}
---
+++ whale-fall-iot-console/src/test/java/org/whalefall/iot/console/WhaleFallIoTConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.whalefall.iot.console;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhaleFallIoTConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
