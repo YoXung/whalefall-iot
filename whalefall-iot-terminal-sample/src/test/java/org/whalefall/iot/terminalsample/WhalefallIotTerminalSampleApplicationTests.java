0c0ab6ff4dcb928a1b952e1378444769305324f9
YaoXiang <89034550@qq.com>
2020-03-24 14:44:03 +0800

    修改工程命名

---
+++ whalefall-iot-terminal-sample/src/test/java/org/whalefall/iot/terminalsample/WhalefallIotTerminalSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.whalefall.iot.terminalsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhalefallIotTerminalSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
