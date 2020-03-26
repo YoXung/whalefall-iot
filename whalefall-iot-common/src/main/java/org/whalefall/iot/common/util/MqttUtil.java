f8eae02ae1a0878c92739ee705d4cc7406a8b6be
YaoXiang <89034550@qq.com>
2020-03-26 17:23:51 +0800

    新增mqtt工具

---
+++ whalefall-iot-common/src/main/java/org/whalefall/iot/common/util/MqttUtil.java
@@ -0,0 +1,11 @@
+package org.whalefall.iot.common.util;
+
+/**
+ * Copyright © 2020 Whale Fall All Rights Reserved
+ *
+ * @author YaoXiang
+ * @description
+ * @create 2020/3/26 5:23 下午
+ */
+public class MqttUtil {
+}
